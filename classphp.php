84cf8ab16e3ddb78d91d6a46b5cfcaa234917238
chia821216 <eve821216@gmail.com>
2024-04-09 15:04:08 +0800

    變數交換

---
+++ classphp.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+echo "ABC";
+
+?>
+<div>
+    $a=10;<br>
+    $b=50;<br>
+</div>
+<h4>a.b.交換後</h4>
+<?php
+$a=10;
+$b=50;
+echo '$a=' . $a ;
+echo "<br>" ;
+echo '$b=' . $b ;
+
+echo "<hr>";
+$score=30;
+$level=($score>=60)?'及格':'不及格';
+echo "成績為:".$score;
+echo "<br>" ;
+echo "是否及格:" . $level ;
+
+
+?>
+</body>
+</html>
