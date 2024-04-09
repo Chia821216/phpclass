16dcd9191a625332c97a45f547b50fefcb52b9e2
chia821216 <eve821216@gmail.com>
2024-04-09 15:27:12 +0800

    自己練習if else

--- classphp.php
+++ classphp.php
@@ -28,7 +28,22 @@ $level=($score>=60)?'及格':'不及格';
 echo "成績為:".$score;
 echo "<br>" ;
 echo "是否及格:" . $level ;
+echo "<hr>";
 
+?>
+<?php
+$clothprice=100;
+$pantsprice=150;
+if($clothprice>=$pantsprice){
+    echo '衣服價錢:' .$clothprice ;
+    echo "<br>" ;
+    echo '褲子價錢:' .$pantsprice ;
+    echo "<br>" ;
+}
+else{
+    $clothprice++ ;
+    echo '衣服價錢:' .$clothprice;
+}
 
 ?>
 </body>
