d8ea10cd102040b527a48edb682247df8b9a4090
chia821216 <eve821216@gmail.com>
2024-04-15 15:32:27 +0800

    畫正三角形

--- pra03_0415.php
+++ pra03_0415.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>畫星星</title>
+    <style>
+        *{
+            font-family:'courier new', Courier , monosoace;
+        }
+    </style>
 </head>
 <body>
 <h2>以 * 符號為基礎在網頁上排列出下列圖形</h2>
@@ -27,11 +32,23 @@ for($i=$stars; $i>0 ; $i--){
     }
     echo '<br>';
 }
-
-
-
-
 ?>
+<h4>正三角形</h4>
+<?php
+
+for($i=0 ; $i<$stars ; $i++){ 
+    for($k=0 ; $k<=$stars-1-$i ; $k++){
+    echo "&nbsp";
+    }
+    for($j=0 ;$j<$i*2+1 ;$j++){
+        echo "*";
+    }
+    echo '<br>';
+}
+?>
+
+
+
 
 
 
